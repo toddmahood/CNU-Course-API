f26b929d43de51fc75766c5281450fbbe8b14ec9
Todd Mahood <todd.mahood.20@cnu.edu>
2022-03-24 23:29:11 -0400

    raise more exceptions if args are invalid, rename file, create test client, fix length function, fix index, save schedule to file

---
+++ client.py
@@ -0,0 +1,4 @@
+from cnu_schedule import *
+
+if __name__ == "__main__":
+    schedule_obj = CNUCourseAPI("Fall 2022")
--- cnu_schedule.py
+++ cnu_schedule.py
@@ -26,7 +26,7 @@ class CNUCourseAPI:
     def __init__(self, semester, after2014=True):
         """
         Get the current course schedule to be used in subsequent methods
-        :param after2014: (boolean) Specifies whether the course was before or after 2014, defaults to True
+        :param after2014: (bool) Specifies whether the schedule is before or after 2014, defaults to True
         :param semester: (str) Specifies the semester to get schedule for. Can pass arguments like "fall 2022", "spring 2021", "2021 fall", ect.
         :raise: ValueError
         :return: None
@@ -61,22 +61,28 @@ class CNUCourseAPI:
 
         # hacky way to get the first 4 consequtive digits in string
         for index, character in enumerate(semester.lower()):
-            if (index + 3) <= semester.length():
+            if (index + 3) <= (len(semester) - 1):
                 if character.isdigit() and semester[index + 1].isdigit() and semester[index + 2].isdigit() and semester[index + 3].isdigit():
-                    year = semester[index:index+3]
+                    if term == "00":
+                        year = str(int(semester[index:index + 4]) + 1) 
+                    else:
+                        year = semester[index:index+3]
                     break
         
         if year == None or term == None:
-            raise ValueError("Invalid semester argument, please try again. Examples of valid ")
+            raise ValueError("Invalid semester argument, please try again. Examples of valid arguments include: 'fall 2020', '2020 spring', 'summer term 1 2018', 'may 2020', 'summer term 2 2019'")
 
         semesterlist = f"{year}{term}" 
+
+        if type(after2014) != bool:
+            raise ValueError("Invalid after2014 argument, must be of type bool.")
 
         if after2014:
             startyearlist = "2"
         else:
             startyearlist = "1"
 
-        
+        print(semesterlist, startyearlist)
         # Create our requests session, makes it easier to carry over cookies to next request.
         session = requests.Session()
 
@@ -89,38 +95,25 @@ class CNUCourseAPI:
         viewstate_generator = urllib.parse.quote(soup.find(id="__VIEWSTATEGENERATOR")["value"], safe='')
         event_validation = urllib.parse.quote(soup.find(id="__EVENTVALIDATION")["value"], safe='')
 
-        
-
+        # Can possibly add these in init header
         interestlist2 = "Any" # Liberal Learning Core, Honors Program or Writing Intensive Course selection
         # disciplineslistbox = "All+Courses" # Subject selection (not required)
 
-        headers = {
-            'Host': 'navigator.cnu.edu',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'upgrade-insecure-requests': '1',
-            'origin': 'https://navigator.cnu.edu',
-            'content-type': 'application/x-www-form-urlencoded',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-user': '?1',
-            'sec-fetch-dest': 'document',
-            'referer': 'https://navigator.cnu.edu/StudentScheduleofClasses/',
-            'accept-language': 'en-US,en;q=0.9'
-        }
-
+        headers['cache-control'] = 'max-age=0'
+        headers['content-type'] = 'application/x-www-form-urlencoded'
+        headers['referer'] = 'https://navigator.cnu.edu/StudentScheduleofClasses/'
+        
         data = f'__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=&__VIEWSTATE={viewstate}&__VIEWSTATEGENERATOR={viewstate_generator}&__EVENTVALIDATION={event_validation}&startyearlist={startyearlist}&semesterlist={semesterlist}&Interestlist2={interestlist2}&CourseNumTextbox=&Button1=Search'
         response = session.post('https://navigator.cnu.edu/StudentScheduleofClasses/', headers=headers, data=data)
+        
         # Stop here if you just want schedule html
         print("Successfully grabbed schedule html.")
-
+        with open("schedule.html", "w+", encoding='UTF-8') as file:
+            file.write(response.text)
 
 
     def search():
         return None
     
     def get_csv():
+        return None
